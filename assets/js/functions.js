24939639bda86bdf93da586b05a71e37f5ac9e8d
jtbryan <jonnib24@gmail.com>
2020-07-13 00:25:07 -0400

    Testing JS

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,13 @@
+$(document).ready(function() {
+    $.each($('.images-right img'), function() {
+        $(this).hide()
+    })
+})
+
 $(window).scroll(function() {
     $.each($('.images-right img'), function() {
-        if ( $(this).offset().top < ($(window).scrollTop() + $(window).height() + 100) ) {
-            $(this).hide()
+        if ( $(this).is(":hidden") && $(this).offset().top < ($(window).scrollTop() + $(window).height() + 100) ) {
+            $(this).show("slide", { direction: "left" }, 1500)
         }
     })
 })
