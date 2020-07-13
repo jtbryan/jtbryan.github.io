8da142b738db9d06f9158419eed584834db37e91
jtbryan <jonnib24@gmail.com>
2020-07-13 00:04:08 -0400

    Fixed descriptions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,7 @@
 $(window).scroll(function() {
     $.each($('.images-right img'), function() {
-        if ( $(this).attr('data-src') && $(this).offset().top < ($(window).scrollTop() + $(window).height() + 100) ) {
-            var source = $(this).data('src');
-            $(this).attr('src', source);
-            $(this).removeAttr('data-src');
+        if ( $(this).offset().top < ($(window).scrollTop() + $(window).height() + 100) ) {
+            console.log("Hey")
         }
     })
 })
